9a267a160abc38b083c795abad460758ea841ad8
Anuj Mishra <anujmishra9893@gmail.com>
2026-03-05 18:24:28 +0530

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://www.aadugaadu.com',
   credentials: true,
 }));
 app.use(express.json());
